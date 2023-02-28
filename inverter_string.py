01f1a7f445e2f22c6e6e6c24465feb0d0fd6323b
taizaoliveira <taizapauladeoliveiralima@gmail.com>
2023-02-28 16:27:02 -0400

    first commit

--- inverter_string.py
+++ inverter_string.py
@@ -1,6 +1,5 @@
-string = input("Digite uma string: ")  # recebe a string do usuário
+string = input("Digite uma string: ")  
 
-# inverte os caracteres da string usando um loop for
 string_invertida = ""
 for i in range(len(string)-1, -1, -1):
     string_invertida += string[i]
--- percentual_representacao.py
+++ percentual_representacao.py
@@ -1,4 +1,3 @@
-# definindo o faturamento mensal por estado
 faturamento = {
     'SP': 67836.43,
     'RJ': 36678.66,
@@ -7,14 +6,11 @@ faturamento = {
     'Outros': 19849.53
 }
 
-# calculando o valor total mensal da distribuidora
 total = sum(faturamento.values())
 
-# calculando o percentual de representação de cada estado
 percentuais = {}
 for estado, valor in faturamento.items():
     percentuais[estado] = (valor / total) * 100
 
-# exibindo os resultados
 for estado, percentual in percentuais.items():
     print(f'{estado}: {percentual:.2f}%')
